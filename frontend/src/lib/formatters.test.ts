0712395811ab50656fb21e7ac4d1c4693b2ae7ad
austinxduong <austinxduong@gmail.com>
2026-09-03 14:32:26 -0700

    create mock testing for timeAgo function

--- frontend/src/lib/formatters.test.ts
+++ frontend/src/lib/formatters.test.ts
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
-import { getStageDotColor } from '@/lib/formatters'
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import { getStageDotColor, timeAgo } from '@/lib/formatters'
+import { vi } from 'vitest'
 
 describe('returns stage colors',() => {
     it('returns bg-pink-500 for New', () =>{
@@ -20,5 +20,58 @@ describe('returns stage colors',() => {
     })
     it('returns undefined for unmatched stages', () => {
         expect(getStageDotColor("Uknown Stage")).toBe(undefined)
+    })
+})
+
+describe('returns last activity at', () => {
+    beforeEach(() => {
+        vi.useFakeTimers()
+    })
+    afterEach(() => {
+        vi.useRealTimers()
+    })
+    it('returns the last time an action activity occured less than 60 seconds ago', () => {
+
+        const mockNow = new Date('2026-09-03T12:00:00Z')
+        vi.setSystemTime(mockNow)
+        console.log("mock now", mockNow)
+
+        const tenSecondsAgo = new Date('2026-09-03T11:59:50Z').toISOString() // "2026-09-03T11:59:50.000Z"
+
+        expect(timeAgo(tenSecondsAgo)).toBe('just now')
+        console.log("ten seconds ago", tenSecondsAgo)
+    })
+    it('returns last time an action activity 10 mintues ago', () => {
+        
+        const mockNow = new Date ('2026-09-03T12:00:00Z')
+        vi.setSystemTime(mockNow)
+
+        const tenMinsAgo = new Date('2026-09-03T11:49:50Z').toISOString()
+        
+        expect(timeAgo(tenMinsAgo)).toBe("10 minutes ago")
+    })
+    it('returns last time an action activiity hours ago', () => {
+
+        const mockNow = new Date('2026-09-03T12:00:00Z')
+        vi.setSystemTime(mockNow)
+
+        const oneHourAgo = new Date('2026-09-03T10:59:50Z').toISOString()
+        expect(timeAgo(oneHourAgo)).toBe("1 hour ago")
+    })
+    it('returns last time an action activity days ago', () => {
+
+        const mockNow = new Date('2026-09-03T12:00:00Z')
+        vi.setSystemTime(mockNow)
+
+        const oneDayAgo = new Date('2026-09-02T10:59:50Z').toISOString()
+        expect(timeAgo(oneDayAgo)).toBe("1 day ago")
+    })
+    it('returns last time an action activity weeks ago', () => {
+        
+        const mockNow = new Date('2026-09-03T12:00:00Z')
+        vi.setSystemTime(mockNow)
+
+        const fourWeeksAgo = new Date('2026-08-02T10:59:50Z').toISOString()
+        expect(timeAgo(fourWeeksAgo)).toBe("4 weeks ago")
     })
 })
