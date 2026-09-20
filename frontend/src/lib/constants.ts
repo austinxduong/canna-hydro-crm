96b7b9d749caf8951e14b08da26f7ccaae805ff5
austinxduong <austinxduong@gmail.com>
2026-09-19 22:02:24 -0700

    Add STATS_URL and ACTIVITY_FEED_URL constants

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -2,3 +2,5 @@ export const PIPELINE_STAGES = ['New', 'Contacted', 'Demo Scheduled', 'Customer'
 export const CATEGORIES = ['Dispensaries', 'Hydroponics'];
 export const SERVER_URL = 'https://canna-hydro-crm.onrender.com/businesses/'
 export const USERS_URL = 'https://canna-hydro-crm.onrender.com/users'
+export const STATS_URL = 'https://canna-hydro-crm.onrender.com/dashboard/stats'
+export const ACTIVITY_FEED_URL = 'https://canna-hydro-crm.onrender.com/dashboard/activityFeed'
